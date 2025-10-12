629a4edf47035f0fb63395ed0c22c7dfc5b2b69b
julian <julianoberhofer2@gmail.com>
2025-10-13 00:03:39 +0200

    fix: enhance QR code handling and improve logging for expiration

--- src/client_on/qr.js
+++ src/client_on/qr.js
@@ -1,18 +1,23 @@
 const qrcode = require('qrcode-terminal');
 
 let qrCodeTries = 0;
+let time = null;
 
 module.exports = {
     event: 'qr',
     handler:  (qr, client, config) => {
         console.log('QR Code received, generating terminal QR code...');
         if (qrCodeTries === 0) {
+                time = new Date()
                 console.log('QR Code received, generating terminal QR code...');
                 console.log('Security Note: Do not share this QR code with anyone.');
                 console.log('Only scan this QR code if you trust this project.');
                 console.log('If you suspect suspicious activity, untrust this session in WhatsApp.');
         } else {
-            console.log('New QR Code received, generating terminal QR code...');
+            const elapsed = Math.floor((new Date() - time) / 1000);
+            console.log(`Previous QR code expired after ${elapsed} seconds.`);
+            time = new Date();
+            console.log(`New QR Code received (try ${qrCodeTries + 1}), generating terminal QR code...`);
         }
         qrCodeTries++;
         qrcode.generate(qr, { small: true });
